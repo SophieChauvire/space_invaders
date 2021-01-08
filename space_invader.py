1da30478720f60a6eda04de5a57cd859e667dd86
tasnimdk <tasnim.dekkiche@cpe.fr>
2021-01-08 11:01:26 +0100

    test avec classes (non fonctionnel pour l'instant)

--- space_invader.py
+++ space_invader.py
@@ -1,9 +1,5 @@
 from tkinter import Tk, Label, Entry, Button, Canvas
 from tkinter import Message
-from fonctions_space_invaders import *
-import space_invaders_fonctions as sif
-
-
 
 fenetre_jeu = Tk() #on crée la fenêtre
 fenetre_jeu.title('Space Invaders')
@@ -13,30 +9,44 @@ affichage_score = Label(fenetre_jeu, text = "Score : ", fg = "black")
 affichage_score.pack() 
 
 
-bouton_debut = Button(Canevas, text = "Démarrer la partie", fg = 'red') #on rajoutera la commande du bouton ensuite 
-bouton_debut.pack(side = "left",padx=300,pady=300)
+#bouton_debut = Button(Canevas, text = "Démarrer la partie", fg = 'red') #on rajoutera la commande du bouton ensuite 
+#bouton_debut.pack(side = "left",padx=300,pady=300)
 bouton_quitter = Button(Canevas,text = "Quitter le jeu",fg = 'red', command = fenetre_jeu.destroy)
 bouton_quitter.pack(side = "left", padx=100, pady=100)
 #reprendre la mise en forme / disposition
 
-#création vaisseau
-#dimensions du vaisseau
 largeur_v = 100
 hauteur_v = 150
+posi_vx = 500
+posi_vy = 750
 
-#position initiale du vaisseau
-x = 500
-y = 570
 
-vaisseau = Canevas.create_rectangle(x-10,y-10,x+10,y+10,width=30,outline="white",fill="white")
+#création vaisseau spatial
+vaisseau_spatial = Canevas.create_rectangle(posi_vx - 10, posi_vy -10, posi_vx +10, posi_vy +10,width=30,outline="white",fill="white")
+
+class vaisseau:
+    def _init_(self):
+        self.largeur_v = largeur_v
+        self.hauteur_v = hauteur_v
+        self.VX = posi_vx
+        self.VY = posi_vy
+        #initialisation d'un élement vaisseau
+        
+    def dep_clavier(event): #méthode déplacement de la classe vaisseau
+        global VX,VY
+        touche = event.keysym
+        if touche == "Right":
+            VX = VX +15
+            if touche == "left":
+                VY = VY-15
+#on re affiche le vaisseau Ã  sa nouvelle position
+        Canevas.coords(vaisseau_spatial,VX-10,VY-10,VX+10,VY+10)
+
+
+
 Canevas.focus_set()
-Canevas.bind("<Key>", dep_clavier)
-Canevas.pack(padx = 3, pady=3)
-
-alien = Canevas.create_rectangle( sif.X0 , sif.Y0 , sif.X1 , sif.Y1 , fil = 'purple')
-
-sif.deplacement_alien()
-
-fenetre_jeu.mainloop()
+Canevas.bind("Right", vaisseau.dep_clavier)
+Canevas.bind("Left", vaisseau.dep_clavier)
+Canevas.pack(padx = 5, pady=5)
 
 fenetre_jeu.mainloop()
