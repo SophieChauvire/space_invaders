4063ed45fb4bd16d769d57139f71a58171d32ba7
SophieChauvire <72541889+SophieChauvire@users.noreply.github.com>
2021-01-17 22:21:40 +0100

    Update space_invaderfinal.py

--- space_invaderfinal.py
+++ space_invaderfinal.py
@@ -8,6 +8,210 @@ Created on Sun Jan 17 18:48:15 2021
 from tkinter import Tk, Label, Entry, Button, Canvas, Message, PhotoImage
 import math,random
 
+class jeu:
+    """
+    Classe de la fenêtre de jeu
+    """
+    def __init__(self, window, length, height, speed = 1,cadence_tir = 4000):
+
+        self.window = window # Fenetre est une fenêtre Tk()
+        self.window.title('Space Invaders')
+        self.window.geometry("600x600")
+        self.window.resizable(width=False, height=False)
+
+        self.canvas = tk.Canvas(self.window, bg = 'black', bd= 0, highlightthickness=0, height = 600, width = 600)
+
+        self.player = Player(self.canvas, self)
+
+        self.length = length
+        self.height = height
+        self.speed = speed
+        self.cadence_tir = cadence_tir  
+
+        self.niveau = 1
+        self.niveau_SV = tk.StringVar()
+        self.niveau_SV.set(str(self.niveau))
+
+        self.bataillon = Bataillon(self.canvas, self, self.length, self.height, self.speed,self.cadence_tir)
+        self.walls = Walls(self.canvas,self)
+        self.game_over = False
+        self.current_tirs = []
+
+        self.menu = Menu()        
+
+        self.transition = False
+
+        self.score = 0
+        self.highscore_SV = tk.StringVar()   # On utilise une stringvariable pour pouvoir changer sa valeur ensuite
+        self.temp = open("highscore.txt", "rt")
+        self.highscore = int(self.temp.readline())  # Entier qui stocke le highscore, pas le même type de variable que highscore_SV
+        self.temp.close()        
+        self.temp = open("highscore.txt", "rt")  # On ouvre deux fois le fichiers au lieu d'une car cela génère une erreur de faire les deux manipulations en une fois
+        self.highscore_SV.set('HIGHSCORE : '+self.temp.readline())
+        self.temp.close()
+       
+        
+
+    def menu_launch(self): # Première méthode appelée quand le jeu est lancé
+        self.canvas.pack(anchor='nw')
+        self.background_display = self.canvas.create_image(300,300,image=self.menu.background)
+        self.logo_display = self.canvas.create_image(300,133,image=self.menu.logo)
+        self.play_button_display =self.canvas.create_window(300,350,window = self.menu.play_button)
+        self.menu.play_button.config( command=self.start)
+        self.exit_button_display = self.canvas.create_window(300,450,window = self.menu.exit_button)
+        self.menu.exit_button.config( command=self.window.destroy)
+        self.highscore_label = tk.Label(self.canvas, textvariable=self.highscore_SV, fg='white', bg='black', font='Helvetica 16 bold')
+        self.highscore_disp = self.canvas.create_window(490,15,window = self.highscore_label)
+        self.canvas.after(16, self.highscore_display)
+        self.sv = tk.StringVar()
+        self.sv.set('SCORE : '+str(self.score))
+        self.score_label = tk.Label(self.canvas, textvariable=self.sv, fg='white', bg='black', font='Helvetica 16 bold')
+    
+    def start(self): # Est appelée pour commencer à jouer, supprime les éléments du menu et lancer les methodes qui permettent le bon déroulement du jeu
+        self.canvas.delete(self.play_button_display,self.exit_button_display,self.background_display,self.logo_display)
+        self.canvas.pack(anchor='nw')
+        
+        self.score_display = self.canvas.create_window(90,15,window = self.score_label)
+        
+        self.canvas.after(16, self.bataillon.deplacement_bataillon)
+        self.canvas.after(16, self.player.player_movement)
+        self.canvas.after(16, self.tirs_management)
+        self.canvas.after(16, self.score_management)
+        self.canvas.after(16, self.bataillon.nouveau_tir)
+        self.canvas.after(16, self.player.no_tir)
+        self.window.bind('<q>', self.player.move_left)
+        self.window.bind('<Q>', self.player.move_left)
+        self.window.bind('<d>', self.player.move_right)
+        self.window.bind('<D>', self.player.move_right)
+        self.window.bind('<KeyRelease>', self.player.stop_move)
+        self.window.bind('<space>', self.player.nouveau_tir)
+    
+    def score_management(self): # Affichage du score pendant la partie
+        self.canvas.delete(self.score_display)
+        self.sv.set('SCORE : '+str(self.score))        
+        self.score_display = self.canvas.create_window(90,15,window = self.score_label)
+        self.canvas.after(16, self.score_management)
+        
+    def highscore_display(self): # Gère la modification en direct de la valeur du highscore après une partie
+        self.canvas.delete(self.highscore_disp)
+        self.temp = open("highscore.txt", "rt")
+        self.highscore_SV.set('HIGHSCORE : '+self.temp.readline())
+        self.temp.close()
+        self.highscore_disp = self.canvas.create_window(490,15,window = self.highscore_label)
+
+    def niveau_screen(self): # Ecran de transition entre deux manches
+        self.niveau += 1
+        self.niveau_SV.set('NIVEAU '+ str(self.niveau))
+        self.niveau_label = tk.Label(self.canvas, textvariable=self.niveau_SV, fg='#FFE213', bg='black', font='Helvetica 60 bold')
+        self.niveau_display = self.canvas.create_window(300,250,window = self.niveau_label)
+        self.canvas.after(1000,self.new_niveau)  #lancement de la prochaine manche
+
+    def nouveau_niveau(self):
+        self.canvas.delete(self.niveau_display)
+        
+        if self.speed < 3 :  # Augmentation de la vitesse jusqu'à un certain seuil
+            self.speed += 0.5
+        if self.cadence_tir > 2100 : # pareil pour la fréquence de tir
+            self.cadence_tir -= 900
+        if self.cadence_tir > 500 and self.cadence_tir < 2100:
+            self.cadence_tir -= 300        
+        self.bataillon = Bataillon(self.canvas, self, self.length, self.height, self.speed,self.cadence_tir)
+        self.transition = False
+        self.canvas.after(16, self.bataillon.deplacement_bataillon)
+        self.canvas.after(16, self.bataillon.nouveau_tir)
+        self.canvas.after(16, self.tirs_management)
+
+    def end_jeu(self): # Ecran de jeu over
+        self.canvas.delete('all')
+        label = tk.Label(self.canvas, text='jeu OVER', fg='white', bg='black')
+        label.config(font=("Liberation", 30))
+        self.canvas.create_window(300, 300, window=label)
+        self.background_display = self.canvas.create_image(300,300,image=self.menu.background)
+        
+        if self.score > self.highscore: # Sauvegarde du meilleur score si on bat le record
+            temp = open("highscore.txt", "wt")
+            temp.write(str(self.score))
+            temp.close()
+        
+        self.canvas.after(500, self.relaunch)
+        
+    def relaunch(self): # Réinitialisation du jeu pour ré-afficher le menu et relancer une partie.
+        self.canvas.delete('all')
+        self.speed = 1
+        self.cadence_tir = 4000
+        self.niveau = 1
+        self.player = Player(self.canvas, self)
+        self.bataillon = Bataillon(self.canvas, self, self.length, self.height, self.speed)
+        self.walls = Walls(self.canvas,self)
+        self.game_over = False
+        self.current_tirs = []
+        self.score = 0
+        
+        self.menu = Menu()
+        self.play_button = tk.Button(self.window, text = 'PLAY !',height = 4, width = 20,command=self.start,activebackground='#ffbd33',background='#FFE213')
+        self.exit_button = tk.Button(self.window, text = 'EXIT',height = 2, width = 10,command=self.window.destroy,activebackground='#ffbd33',background='#FFE213')
+        
+        jeu.menu_launch()
+
+    def tirs_management(self): # Gestion du mouvement des lasers et de la collision, ainsi que de l'augmentation de la variable score
+        if self.game_over:
+            return
+        for tir in self.current_tirs:
+            touch = False  # True si l'entité est touchée
+            index_alien_to_delete = None
+            index_block_to_delete = None
+            index_alien_to_touch = None
+
+            self.canvas.move(tir.sprite, 0,tir.direction*4)
+            if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(self.player.sprite)[0]) < 20 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(self.player.sprite)[1]) < 33 and tir.direction == 1:
+                
+                touch = True
+                self.game_over = True
+                
+            for alien in self.bataillon.liste_alien:
+                if abs(self.canvas.coords(tir.sprite)[0] - self.canvas.coords(alien.sprite)[0]) < 23 and abs(self.canvas.coords(tir.sprite)[1] - self.canvas.coords(alien.sprite)[1]) < 33 and tir.direction == -1:
+                    
+                    touch = True
+                    if alien.pv == 1:
+                        index_alien_to_delete = self.bataillon.liste_alien.index(alien)  # Suppression de l'ennemi mort
+                        self.score += alien.score  # Augmentation du score quand on tue un ennemi
+                    else:
+                        alien.pv -= 1  # On baisse la vie de l'ennemi touché
+                        index_alien_to_touch = self.bataillon.liste_alien.index(alien)
+               
+
+            for block in self.walls.blocks_list: # Gestion de la destruction des blocs
+                if self.canvas.coords(block.sprite)[0] - self.canvas.coords(tir.sprite)[0] < 29 and self.canvas.coords(block.sprite)[0] - self.canvas.coords(tir.sprite)[0] > 0 and self.canvas.coords(tir.sprite)[1] - self.canvas.coords(block.sprite)[1] < 9 and self.canvas.coords(block.sprite)[1] - self.canvas.coords(tir.sprite)[1] < 29:
+                    touch = True
+                    index_block_to_delete = self.walls.blocks_list.index(block)
+            
+            if index_alien_to_touch != None:
+                temp = self.bataillon.liste_alien[index_alien_to_touch]
+                self.canvas.itemconfig(temp.sprite, image=temp.set_image[temp.kind-1+len(temp.set_image)//2]) # La liste est pensée telle que la 2e moitié puisse servir à representer la premiere moitié blessée
+            if index_alien_to_delete != None:
+                self.canvas.delete(self.bataillon.liste_alien[index_alien_to_delete].sprite)
+                self.bataillon.liste_alien.remove(self.bataillon.liste_alien[index_alien_to_delete])
+            if index_block_to_delete != None:
+                self.canvas.delete(self.walls.blocks_list[index_block_to_delete].sprite)
+                self.walls.blocks_list.remove(self.walls.blocks_list[index_block_to_delete])
+            
+            if self.canvas.coords(tir.sprite)[1] >= 560 or self.canvas.coords(tir.sprite)[1] < 0 or touch == True:
+                self.canvas.delete(tir.sprite)
+                self.current_tirs.remove(tir)
+            
+            if self.game_over == True:
+                self.canvas.delete(self.player.sprite)
+                self.canvas.after(1000, self.end_jeu)
+                return
+
+            if len(self.bataillon.liste_alien) == 0:
+                self.transition = True
+            if len(self.bataillon.liste_alien) == 0 and len(self.current_tirs) == 0:
+                self.canvas.after(1000, self.niveau_screen) # on lance la prochaine manche
+                return
+                
+        self.canvas.after(16, self.tirs_management)
+
 class alien:
     def __init__(self, jeu, canvas, posi_x=6, posi_y=1,sens = 1,vitesse = 2,nombre = 16, niv = 2):
         self.jeu = jeu
